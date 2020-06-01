22eb9b672cd7c60aae972ec77feb44a6d16faa64
Lluís <lluis.revilla@gmail.com>
2020-06-01 11:19:08 +0200

    Render files, add them, commit and push

--- last.R
+++ last.R
@@ -1,2 +1,7 @@
-rmarkdown::render("last_BORME.Rmd", quiet = TRUE, output_dir = "docs", clean = TRUE)
-rmarkdown::render("last_BOE.Rmd", quiet = TRUE, output_dir = "docs", clean = TRUE)
+rmarkdown::render(c("last_BORME.Rmd", "last_BOE.Rmd"),
+                  quiet = TRUE, output_dir = "docs", clean = TRUE)
+git2r::add(path = c(
+    file.path("docs", c("last_BOE.html", "last_BORME.html"))),
+    file.path("docs", "site_libs", "*"))
+git2r::commit(message = "Automatic commit to update last BOE and BORME")
+git2r::push()
