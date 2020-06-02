ed1303be15d24e5bdc0efc6eb17119b913b89666
Lluís <lluis.revilla@gmail.com>
2020-06-02 09:38:51 +0200

    Correct some errors on the script

--- last.R
+++ last.R
@@ -1,7 +1,10 @@
-rmarkdown::render(c("last_BORME.Rmd", "last_BOE.Rmd"),
+setwd("/home/lluis/Documents/Projects/Explore_BOE/")
+rmarkdown::render("last_BORME.Rmd",
+                  quiet = TRUE, output_dir = "docs", clean = TRUE)
+rmarkdown::render("last_BOE.Rmd",
                   quiet = TRUE, output_dir = "docs", clean = TRUE)
 git2r::add(path = c(
-    file.path("docs", c("last_BOE.html", "last_BORME.html"))),
-    file.path("docs", "site_libs", "*"))
+    file.path(".", "docs", c("last_BOE.html", "last_BORME.html")),
+    file.path(".", "docs", "site_libs", "*")))
 git2r::commit(message = "Automatic commit to update last BOE and BORME")
 git2r::push()
