b5bfef90dfce3b7ddd747288c0aaf5b195cada14
Lluís <lluis.revilla@gmail.com>
2020-11-09 12:29:10 +0100

    Fix app name

--- last_BORME.R
+++ last_BORME.R
@@ -1,6 +1,6 @@
 library("rtweet")
 token <- rtweet::create_token(
-    app = "BOE & BORME",
+    app = "Boletines Españoles",
     consumer_key =    Sys.getenv("TWITTER_API_KEY"),
     consumer_secret = Sys.getenv("TWITTER_API_SECRET_KEY"),
     access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
--- news_BOE.R
+++ news_BOE.R
@@ -1,7 +1,7 @@
 library('BOE')
 library("rtweet")
 token <- rtweet::create_token(
-    app = "BOE & BORME",
+    app = "Boletines Españoles",
     consumer_key =    Sys.getenv("TWITTER_API_KEY"),
     consumer_secret = Sys.getenv("TWITTER_API_SECRET_KEY"),
     access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
