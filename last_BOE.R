aee42546b174145071568ed7bc0614000302f0d0
Lluís <lluis.revilla@gmail.com>
2020-11-22 10:44:24 +0100

    Check if there is need to update today

--- last_BOE.R
+++ last_BOE.R
@@ -1,4 +1,14 @@
 library("rtweet")
+library("BOE")
+today <- Sys.Date()
+url_sumario <- query_xml(sumario_xml(today, "BOE"))
+
+valid_url <- function(url_in, t = 2){
+    con <- url(url_in)
+    r <- readLines(con, n = 2, encoding = "UTF-8")
+    close(con)
+    !grepl("No se encontr&#xF3; el sumario original.", r[2])
+}
 token <- rtweet::create_token(
     app = "Boletines Españoles",
     consumer_key =    Sys.getenv("TWITTER_API_KEY"),
@@ -6,4 +16,6 @@ token <- rtweet::create_token(
     access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
     access_secret =   Sys.getenv("TWITTER_ACCESS_SECRET_TOKEN")
 )
-rmarkdown::render_site("last_BOE.Rmd")
+if (valid_url(url_sumario)) {
+    rmarkdown::render_site("last_BOE.Rmd")
+}
