9ff587961d29a986f7eaa6ec689796b2fde6cedb
Lluís <lluis.revilla@gmail.com>
2022-12-17 17:29:00 +0100

    Update to fix problems?

--- last_BOE.R
+++ last_BOE.R
@@ -3,13 +3,6 @@ library("BOE")
 today <- Sys.Date()
 httr::set_config(httr::config(ssl_verifypeer = FALSE))
 url_sumario <- query_xml(sumario_xml(today, "BOE"))
-
-valid_url <- function(url_in, t = 2){
-    con <- url(url_in)
-    r <- readLines(con, n = 2, encoding = "UTF-8")
-    close(con)
-    !grepl("No se encontr&#xF3; el sumario original.", r[2])
-}
 token <- rtweet::rtweet_bot(
     api_key =    Sys.getenv("TWITTER_API_KEY"),
     api_secret = Sys.getenv("TWITTER_API_SECRET_KEY"),
@@ -17,6 +10,4 @@ token <- rtweet::rtweet_bot(
     access_secret =   Sys.getenv("TWITTER_ACCESS_SECRET_TOKEN")
 )
 rtweet::auth_as(token)
-if (valid_url(url_sumario)) {
-    rmarkdown::render_site("last_BOE.Rmd")
-}
+rmarkdown::render_site("last_BOE.Rmd")
--- last_BORME.R
+++ last_BORME.R
@@ -1,16 +1,7 @@
 library("rtweet")
 library("BOE")
 today <- Sys.Date()
-httr::set_config(httr::config(ssl_verifypeer = FALSE))
 url_sumario <- query_xml(sumario_xml(today, "BORME"))
-
-valid_url <- function(url_in, t = 2){
-    con <- url(url_in)
-    r <- readLines(con, n = 2, encoding = "UTF-8")
-    close(con)
-    !grepl("No se encontr&#xF3; el sumario original.", r[2])
-}
-
 token <- rtweet::rtweet_bot(
     api_key =    Sys.getenv("TWITTER_API_KEY"),
     api_secret = Sys.getenv("TWITTER_API_SECRET_KEY"),
@@ -18,6 +9,4 @@ token <- rtweet::rtweet_bot(
     access_secret =   Sys.getenv("TWITTER_ACCESS_SECRET_TOKEN")
 )
 rtweet::auth_as(token)
-if (valid_url(url_sumario)) {
-    rmarkdown::render_site("last_BORME.Rmd")
-}
+rmarkdown::render_site("last_BORME.Rmd")
