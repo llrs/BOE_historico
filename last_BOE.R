eb817b935e139da98df47729fcd284780f991685
Lluís <lluis.revilla@gmail.com>
2023-07-13 19:24:32 +0200

    Remove twitter

--- last_BOE.R
+++ last_BOE.R
@@ -1,13 +1,2 @@
-library("rtweet")
 library("BOE")
-today <- Sys.Date()
-httr::set_config(httr::config(ssl_verifypeer = FALSE))
-url_sumario <- query_xml(sumario_xml(today, "BOE"))
-token <- rtweet::rtweet_bot(
-    api_key =    Sys.getenv("TWITTER_API_KEY"),
-    api_secret = Sys.getenv("TWITTER_API_SECRET_KEY"),
-    access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
-    access_secret =   Sys.getenv("TWITTER_ACCESS_SECRET_TOKEN")
-)
-rtweet::auth_as(token)
 rmarkdown::render_site("last_BOE.Rmd")
